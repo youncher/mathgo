f3aeac2c0b029b967ba602018e8c6fe3832a60e7
unknown <trandani@oregonstate.edu>
2020-10-24 18:57:30 -0700

    Firebase with oauth and simple buttons

---
+++ Assets/Scripts/GoogleSignInTest.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+using Firebase;
+using Firebase.Auth;
+using Google;
+using UnityEngine.UI;
+using System.Threading.Tasks;
+
+
+public class GoogleSignInTest : MonoBehaviour
+{
+    public Text infoText;
+    public string webClientId = "<your client id here>";
+
+    private FirebaseAuth auth;
+    private GoogleSignInConfiguration configuration;
+
+    void Awake()
+    {
+        configuration = new GoogleSignInConfiguration
+        {
+            WebClientId = webClientId, 
+            RequestEmail = true,
+            RequestIdToken = true
+        };
+        CheckFirebaseDependencies();
+    }
+
+    private void CheckFirebaseDependencies()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            if (task.IsCompleted)
+            {
+                if (task.Result == DependencyStatus.Available)
+                {
+                    auth = FirebaseAuth.DefaultInstance;
+                }
+                else
+                {
+                    AddToInformation("Could not resolve all firebase dependencies: " + task.Result.ToString());
+                }
+            }
+            else
+            {
+                AddToInformation("Dependency check was not completed. Error: " + task.Exception.Message);
+            }
+        });
+    }
+
+    // Called when clicking button from Unity
+    public void SignInWithGoogle() { OnSignIn();  }
+    public void SignOutFromGoogle() { OnSignOut(); }
+
+    private void OnSignIn()
+    {
+        GoogleSignIn.Configuration = configuration;
+        GoogleSignIn.Configuration.UseGameSignIn = false;
+        GoogleSignIn.Configuration.RequestIdToken = true;
+        infoText.text = "";
+        infoText.text += "Calling SignIn";
+
+        GoogleSignIn.DefaultInstance.SignIn().ContinueWith(
+          OnAuthenticationFinished);
+    }
+
+    private void OnSignOut()
+    {
+        infoText.text = "Calling SignOut";
+        GoogleSignIn.DefaultInstance.SignOut();
+    }
+
+    public void OnDisconnect()
+    {
+        AddToInformation("Calling Disconnect");
+        GoogleSignIn.DefaultInstance.Disconnect();
+    }
+
+    internal void OnAuthenticationFinished(Task<GoogleSignInUser> task)
+    {
+        if (task.IsFaulted)
+        {
+            using (IEnumerator<System.Exception> enumerator =
+                    task.Exception.InnerExceptions.GetEnumerator())
+            {
+                if (enumerator.MoveNext())
+                {
+                    GoogleSignIn.SignInException error =
+                            (GoogleSignIn.SignInException)enumerator.Current;
+                    AddToInformation("Got Error: " + error.Status + " " + error.Message);
+                }
+                else
+                {
+                    AddToInformation("Got Unexpected Exception?!?" + task.Exception);
+                }
+            }
+        }
+        else if (task.IsCanceled)
+        {
+            AddToInformation("Canceled");
+        }
+        else
+        {
+            AddToInformation("Welcome: " + task.Result.DisplayName + "!\nEmail = " + task.Result.Email + 
+                "\nGoogle ID Token = " + task.Result.IdToken);
+
+            SignInWithGoogleOnFirebase(task.Result.IdToken);
+        }
+    }
+
+    private void SignInWithGoogleOnFirebase(string idToken)
+    {
+        Credential credential = GoogleAuthProvider.GetCredential(idToken, null);
+
+        auth.SignInWithCredentialAsync(credential).ContinueWith(task =>
+        {
+            AggregateException ex = task.Exception;
+            if (ex != null)
+            {
+                if (ex.InnerExceptions[0] is FirebaseException inner && (inner.ErrorCode != 0))
+                {
+                    AddToInformation("\nError code = " + inner.ErrorCode + " Message = " + inner.Message);
+                }
+                else
+                {
+                    AddToInformation("Sign in Successful.");
+                }
+            }
+        });
+    }
+
+    public void OnSignInSilently()
+    {
+        GoogleSignIn.Configuration = configuration;
+        GoogleSignIn.Configuration.UseGameSignIn = false;
+        GoogleSignIn.Configuration.RequestIdToken = true;
+        AddToInformation("Calling SignIn Silently");
+
+        GoogleSignIn.DefaultInstance.SignInSilently()
+              .ContinueWith(OnAuthenticationFinished);
+    }
+
+    public void OnGamesSignIn()
+    {
+        GoogleSignIn.Configuration = configuration;
+        GoogleSignIn.Configuration.UseGameSignIn = true;
+        GoogleSignIn.Configuration.RequestIdToken = false;
+
+        AddToInformation("Calling Games SignIn");
+
+        GoogleSignIn.DefaultInstance.SignIn().ContinueWith(
+          OnAuthenticationFinished);
+    }
+
+    private void AddToInformation(string str) { infoText.text += "\n" + str; }
+}
