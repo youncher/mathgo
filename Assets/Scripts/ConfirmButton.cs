4c0759ca94fc07f3c68752465739c106133c6c88
Jonathan Abantao <jonabantao@gmail.com>
2020-10-25 16:36:34 -0700

    Add character select screen

---
+++ Assets/Scripts/ConfirmButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfirmButton : MonoBehaviour
+{
+    public Button confirmButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        confirmButton.enabled = false;
+        confirmButton.interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool shouldBeEnabled = UIManager.Instance.ShouldEnableConfirmButton();
+        
+        if (shouldBeEnabled && confirmButton.enabled == false)
+        {
+            Debug.Log("Enabling Confirm Button");
+            confirmButton.enabled = true;
+            confirmButton.interactable = true;
+        }
+        else if (!shouldBeEnabled && confirmButton.enabled == true)
+        {
+            Debug.Log("Disabling Confirm Button");
+            confirmButton.enabled = false;
+            confirmButton.interactable = false;
+        }
+    }
+
+    public void HandleRegistration()
+    {
+        UIManager.Instance.HandleRegistration();
+    }
+}
---
+++ Assets/Scripts/SignOnButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignOnButton : MonoBehaviour
+{
+    public Canvas SignOnCanvas;
+    public Canvas NewCharacterCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HandleSignOn()
+    {
+        // TODO: Add google auth - if character already exists in database, goto map scene, else, new character screen displayed
+        ChangeToNewCharacterScreen();
+    }
+
+    public void ChangeToNewCharacterScreen()
+    {
+        SignOnCanvas.enabled = false;
+        NewCharacterCanvas.enabled = true;
+    }
+}
