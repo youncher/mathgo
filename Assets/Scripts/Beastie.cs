9c14e6b56addcc02879f93e16936bba2ffb8855f
Jonathan Abantao <jonabantao@gmail.com>
2020-10-29 22:08:58 -0700

    Create beastie factory for map view scene

---
+++ Assets/Scripts/Beastie.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: Currently a placeholder to ensure only Beasties are created from Factory
+public class Beastie : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/BeastieFactory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeastieFactory : MonoBehaviour
+{
+    [SerializeField]
+    private Beastie[] beasties;
+
+    private static BeastieFactory _instance;
+    public static BeastieFactory Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("No BeastieFactory instance found!");
+            }
+
+            return _instance;
+        }
+    }
+
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public Beastie GenerateBeastie()
+    {
+        int index = Random.Range(0, beasties.Length);
+        return beasties[index];
+    }
+}
