008c353fea59f1ff0219773c3eb6707ac3da6a69
unknown <dtran550@gmail.com>
2020-11-11 22:21:07 -0800

    Basic Capture UI

---
+++ Assets/Scripts/CaptureManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureManager : MonoBehaviour
+{
+    [SerializeField] private GameObject Question;
+    [SerializeField] private GameObject A;
+    [SerializeField] private GameObject B;
+    [SerializeField] private GameObject C;
+    [SerializeField] private GameObject D;
+    private float Answer;
+
+    // Awake is called when the script instance is being loaded
+    private void Awake()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
