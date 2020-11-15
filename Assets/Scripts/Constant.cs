6414bb4c799a2612600eba77f23a1e8924fe2af2
Cherie Young <44423254+youncher@users.noreply.github.com>
2020-11-15 03:46:20 -0800

    Update api uri to use constants

--- Assets/Scripts/Constant.cs
+++ Assets/Scripts/Constant.cs
@@ -3,4 +3,7 @@
     public const string SignOnScene = "SignOnScene";
     public const string CaptureScene = "CaptureScene";
     public const string OverworldMap = "OverworldMap";
+    
+    public const string UserValidationUri = "http://mathgo-46d6d.wl.r.appspot.com/user/validation";
+    public const string MathProblemsUri = "http://mathgo-46d6d.wl.r.appspot.com/math-problems";
 }
