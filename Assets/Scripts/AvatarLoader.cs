aef3a949a18f5e1ac42ba9c3024e33e9a932a2cb
Jonathan Abantao <jonabantao@gmail.com>
2020-11-29 14:48:15 -0800

    Render avatar into overworld map

---
+++ Assets/Scripts/AvatarLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarLoader : MonoBehaviour
+{
+    [SerializeField] private GameObject[] characters;
+
+    void Start()
+    {
+        GameObject loader = GameObject.Find("Loader");
+        GameManager gameManager = loader.GetComponent<GameManager>();
+
+        int charType = gameManager.charType;
+
+        if (charType < characters.Length)
+        {
+            Vector3 avatarPosition = new Vector3(transform.position.x + 5f, transform.position.y - 0.25f, transform.position.z - 5.25f);
+            GameObject player = Instantiate(characters[charType], avatarPosition, Quaternion.Euler(0, 145, 0));
+            player.transform.parent = transform;
+        }
+    }
+}
