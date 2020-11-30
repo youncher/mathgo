23e8df9d930d9448a236989a92709ed5c0dc47f7
Cherie Young <44423254+youncher@users.noreply.github.com>
2020-11-30 00:22:39 -0800

    Add tutorial to map view

---
+++ Assets/Scripts/OverlayCloseButton.cs
@@ -0,0 +1,16 @@
+﻿﻿using UnityEngine;
+
+public class OverlayCloseButton : MonoBehaviour
+{
+    private GameObject overObject;
+    
+    void Start()
+    {
+        overObject = GameObject.Find("TutorialOverlay");
+    }
+
+    public void HandleClickDeactivateOverlay()
+    {
+        overObject.SetActive(false);
+    }
+}
